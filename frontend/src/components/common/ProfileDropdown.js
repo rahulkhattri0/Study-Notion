5245247a37dc92116d353fd5a6492c5638744a5c
Rahul Khattri <rahulkhattri14@gmail.com>
2024-01-02 18:34:05 +0530

    refactored old code in profile dropdown

--- frontend/src/components/common/ProfileDropdown.js
+++ frontend/src/components/common/ProfileDropdown.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {IoMdArrowDropdown} from 'react-icons/io'
 import { VscDashboard, VscSignOut } from "react-icons/vsc"
@@ -6,30 +6,24 @@ import { logout } from '../../services/operations/auth'
 import { Link, useNavigate } from 'react-router-dom'
 const ProfileDropdown = () => {
   const user = useSelector((store)=>store.profile.user)
+  const dropDownRef = useRef()
   const [open,setOpen] = useState(false)
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  const callback = (event) =>{
-    const closest = event.target.closest('#DropDown')
-    console.log("closest",closest)
-    if(!closest){
-      document.removeEventListener("mousedown",callback)
-      setOpen(false)
-    }
- }
-  const handleDropdown = ()=>{
-    if(open) {
-        document.removeEventListener("mousedown",callback)
+  useEffect(()=>{
+    const callback = (event) =>{
+      if(!dropDownRef.current.contains(event.target)){
         setOpen(false)
+      }
     }
-    else{
-        document.addEventListener("mousedown",callback)
-        setOpen(true)
+    if(open){
+      document.addEventListener('click',callback)
     }
-  }
+    return ()=>document.removeEventListener('click',callback)
+  },[open])
   return (
-    <div className='relative' id='DropDown' >
-      <div className='text-richblack-25 flex flex-row gap-x-1 items-center' onClick={handleDropdown}>
+    <div className='relative' ref={dropDownRef}>
+      <div className='text-richblack-25 flex flex-row gap-x-1 items-center' onClick={()=>setOpen((prev)=>!prev)}>
           <img src={user.image} alt={`image of ${user.firstname}`} 
           className="aspect-square w-[30px] rounded-full object-cover"
           />
@@ -38,7 +32,7 @@ const ProfileDropdown = () => {
       {
         open===true && (
           <div className='absolute top-12 right-1 z-[1000] flex flex-col rounded-md border-[1px] border-richblack-700 bg-richblack-800 gap-y-2 cursor-pointer'>
-            <Link to="/dashboard/my-profile" onClick={() => setOpen(false)}>
+            <Link to="/dashboard/my-profile" onClick={()=>setOpen(false)}>
                 <div className='p-2 gap-x-1 rounded-md flex items-center text-richblack-100  hover:bg-richblack-700 hover:text-richblack-25 m-3'>
                 <VscDashboard className='text-lg'/>
                 Dashboard
